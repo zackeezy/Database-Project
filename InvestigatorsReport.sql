6dccc105da346b26564de297493de8ba1e827804
zackeezy <zacaddison25@gmail.com>
2017-04-27 12:49:32 -0500

    Added counts

--- InvestigatorsReport.sql
+++ InvestigatorsReport.sql
@@ -1,4 +1,4 @@
-select i.FirstName, i.LastName, i.CollegeProgram, i.Department, i.Email, pn.AreaCode, p.ProposalID, a.AwardID, pn.Number
+select i.FirstName, i.LastName, i.CollegeProgram, i.Department, i.Email, pn.AreaCode, count(p.ProposalID) as Proposals, count(a.AwardID) as awards, pn.Number
 from Investigator i
 join PhoneInvestigator pi on pi.InvestigatorID = i.InvestigatorID
 join PhoneNumber pn on pi.PhoneNumberID = pn.PhoneNumberID
@@ -6,3 +6,4 @@ join InvestigatorProposal ip on i.InvestigatorID = ip.InvestigatorID
 join Proposal p on p.ProposalID = ip.ProposalID
 join InvestigatorAward ia on ia.InvestigatorID = i.InvestigatorID
 join Award a on a.AwardID = ia.AwardID
+group by i.FirstName, i.LastName, i.CollegeProgram, i.Department, i.Email, pn.AreaCode, pn.Number
